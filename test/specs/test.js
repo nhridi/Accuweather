ba737e7f3315078d119628daa0c81426805af35c
nhridi <n.hridi@a1qa.com>
2024-07-03 18:43:15 +0600

    .gitigonre

--- test/specs/test.js
+++
@@ -1,8 +0,0 @@
-const MainPage = require('../pageobjects/mainPage');
-
-describe('AccuWeather Test Suite', () => {
-    before(async () => {
-        await MainPage.open();
-        await MainPage.consentDataUsage();
-    });
-});
