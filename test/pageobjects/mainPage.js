c677922292517e02820db2577aec5ac70aea7621
nhridi <n.hridi@a1qa.com>
2024-07-03 11:34:17 +0600

    step 01: Consent data usage.

--- test/pageobjects/mainPage.js
+++ test/pageobjects/mainPage.js
@@ -3,12 +3,28 @@ const { config } = require('../../wdio.conf');
 class MainPage {
 
     constructor() {
-        this.url = browser.config.baseUrl;
+        this.url = config.baseUrl;
+        this.consentButtonSelector = '#privacy-policy-banner .banner-button.policy-accept'; 
     }
 
     async open() {
-        await browser.url(this.url);
+        try {
+            await browser.url(this.url);
+        } catch (error) {
+            console.error('Error opening the page:', error);
+        }
     }
- }
+
+    async consentDataUsage() {
+        await browser.waitForDisplayed(this.consentButtonSelector, {
+            timeout: 5000,
+            timeoutMsg: 'Consent button was not displayed in time'
+        });
+        const isConsentButtonVisible = await browser.isDisplayed(this.consentButtonSelector);
+        if (isConsentButtonVisible) {
+            await browser.click(this.consentButtonSelector);
+        }
+    }
+}
  
  module.exports = new MainPage();
--- test/specs/test.js
+++ test/specs/test.js
@@ -3,5 +3,6 @@ const MainPage = require('../pageobjects/mainPage');
 describe('AccuWeather Test Suite', () => {
     before(async () => {
         await MainPage.open();
+        await MainPage.consentDataUsage();
     });
 });
