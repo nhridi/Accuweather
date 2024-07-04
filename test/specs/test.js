fa99fc6ddc942a95d880cc3068f53488c0315281
nhridi <n.hridi@a1qa.com>
2024-07-04 09:33:17 +0600

    created 1 describe and it and put function in it

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,19 +1,21 @@
 import mainPage from '../pageobjects/mainPage.js';
 
 describe('AccuWeather Test Suite', () => {
-    it('consenting to data usage and allow cookie',async () => {
+    it('consenting to data usage and allow cookie, click on the search box and write "New York", select first element of the test result',async () => {
         await mainPage.acceptConsentButton();
+        await mainPage.searchCity();
+        await mainPage.clickFirstresult();
     });
 });
 
-describe('accuWeather Search Suite', () => {
-    it('click on the search box and write "New York"', async () => {
-        await mainPage.searchCity();
-    })
-})
+// describe('accuWeather Search Suite', () => {
+//     it('click on the search box and write "New York"', async () => {
+//         await mainPage.searchCity();
+//     })
+// })
 
-describe('accuWeather Select Suite', () => {
-    it('select first element of the test result', async () => {
-        await mainPage.clickFirstresult();
-    })
-})
+// describe('accuWeather Select Suite', () => {
+//     it('select first element of the test result', async () => {
+//         await mainPage.clickFirstresult();
+//     })
+// })
