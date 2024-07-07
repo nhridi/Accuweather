14d42e52eb3526226d35a66fb07423405957920d
nhridi <n.hridi@a1qa.com>
2024-07-07 18:29:24 +0600

    file download

--- Utils/fileUtils.js
+++ Utils/fileUtils.js
@@ -1,12 +1,13 @@
 import fs from 'fs';
 import { downloadDir } from './environment/directories.js';
 
-const ensureDownloadDirectoryExists = () => {
-  return fs.promises.mkdir(downloadDir, { recursive: true })
-    .catch(error => {
-      console.error(`Failed to ensure that the download directory exists: ${error}`);
-      throw error;
-    });
+const ensureDownloadDirectoryExists = async () => {
+  try {
+        return await fs.promises.mkdir(downloadDir, { recursive: true });
+    } catch (error) {
+        console.error(`Failed to ensure that the download directory exists: ${error}`);
+        throw error;
+    }
 };
 
 export { ensureDownloadDirectoryExists };
