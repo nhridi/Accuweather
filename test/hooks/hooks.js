8872b6ecf3bd4a4572cba99bb1f423be118065db
nhridi <n.hridi@a1qa.com>
2024-07-26 15:19:57 +0600

    All 19 test scenario passed

--- test/hooks/hooks.js
+++ test/hooks/hooks.js
@@ -1,13 +1,13 @@
 import Browser from "../../framework/browser/Browser.js";
 import { mainConfig } from "../../framework/configs/main.wdio.conf.js";
 import { defaultWindowSize } from "../../framework/constants/windowElement.js";
-import {Before, After } from "@wdio/cucumber-framework";
+import {Before, AfterStep, AfterAll } from "@wdio/cucumber-framework";
 
 Before(async () => {
     await Browser.openUrl(mainConfig.baseUrl);
     await Browser.Window.resize(defaultWindowSize);
 });
 
-After(async () => {
+AfterAll(async () => {
     await Browser.Window.closeCurrentWindow();
 });
